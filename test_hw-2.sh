2a3dacbe732e06f662dc19d8b606f9b07876a4dc
SShakhov <k0c0s25@mail.ru>
2014-05-11 04:08:54 +0400

    Update test script for task 5

--- test_hw-2.sh
+++ test_hw-2.sh
@@ -66,6 +66,26 @@ for n in {1..4} ; do
 	done
 	rm $bin 2>/dev/null
 done
+	# Compile task.
+	gcc -m32 -c task_5.s 2>/dev/null
+	gcc -m32 task_5.o task_5.c -o task 2>/dev/null
+	rm task_5.o
+	if [ $? -eq 0 ]; then
+		echo -e "\tTASK $n: ${green}COMPILE OK${NC}"
+	else
+		echo -e "\tTASK $n: ${red}COMPILE FAILED${NC}"
+	fi
 
+	for t in {1..5} ; do
+		eval $bin 2>/dev/null 1>task.out < ./tests/task_5-$t.in
+		diff ./tests/task_5-$t.out task.out 2>/dev/null
+		if [ $? -eq 0 ]; then
+			echo -e "\t\t\t${green}RESULT OK${NC}"
+		else
+			echo -e "\t\t\t${red}RESULT WRONG${NC}"
+		fi
+		rm task.out 2>/dev/null
+	done
+	rm $bin 2>/dev/null
 # Return to origin folder.
 cd ../..
