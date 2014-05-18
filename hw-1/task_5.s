3a19446be89d20e46fe33a396211f0d58fcbabde
SShakhov <k0c0s25@mail.ru>
2014-05-19 02:55:33 +0400

    Add task_5

---
+++ hw-1/task_5.s
@@ -0,0 +1,58 @@
+	.data
+printf_nl:
+	.string	"\n"
+printf_format:
+	.string	"%d"
+scanf_format:
+	.string	"%f"
+
+	.bss
+input:
+	.space	4
+save_ecx:
+	.space	4
+save_eax:
+	.space	4
+
+	.text
+	.globl	main
+
+main:
+//	Prologue
+	pushl	%ebp
+	movl	%esp, %ebp
+
+	pushl	$input
+	pushl	$scanf_format
+	call	scanf
+
+	movl	input, %eax
+	roll	$1, %eax
+	movl	$32, %ecx
+
+
+print_loop:
+	movl	%eax, %ebx
+	andl	$0x1, %ebx
+	pushl	%ebx
+	pushl	$printf_format
+	movl	%eax, save_eax
+	movl	%ecx, save_ecx
+	call	printf
+	movl	save_eax, %eax
+	movl	save_ecx, %ecx
+
+
+	roll	%eax
+	loop	print_loop
+
+
+	pushl	$printf_nl
+	call	printf
+
+
+
+//	Epilogue
+	movl	%ebp, %esp
+	popl	%ebp
+	ret
